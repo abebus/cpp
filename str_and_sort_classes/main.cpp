1f024376cdf4ff28a0387bdcfd3c38bcfd412a05
abebus <anamev263@gmail.com>
2022-11-01 19:26:42 +0300

    ura segfault

--- str_and_sort_classes/main.cpp
+++ str_and_sort_classes/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cassert>
 
 class String{
     friend std::ostream& operator<<(std::ostream&, const String&);
+    //friend String operator[](String&, String&);
 
     char *content_{nullptr};
     int size_{0};
@@ -12,6 +14,12 @@ class String{
     }
 public:
     String() = default;
+    String(char ch, int size){
+        size_ = size;
+        content_ = new char[size];
+        for (int i = 0; i < size; i++)
+            content_[i] = ch;
+    }
     explicit String(int size){
         size_ = size;
         content_ = new char[size];
@@ -27,9 +35,41 @@ public:
         }
     };
     ~String() {
-        size_ = 0;
         delete[] content_;
+        size_ = 0;
     };
+
+    char& operator[](int);
+    String& operator+(String);
+    String& operator+=(String);
+    String& operator=(String);
+
+    bool operator==(String);
+    bool operator>(String);
+    bool operator<(String);
+    bool operator<=(String);
+    bool operator>=(String);
+    bool operator!=(String);
+
+    int len() const{return size_;}
+
+
+    String upper(){
+        String res = String(this->content_,this->size_);
+        for (int i = 0; i < size_; i++){
+            if (this->content_[i] >= 'a' and this->content_[i] <= 'z')
+                res[i] = this->content_[i] - ('a' - 'A');
+        }
+        return res;
+    }
+    String lower(){
+        String res = String(this->content_,this->size_);
+        for (int i = 0; i < size_; i++){
+            if (this->content_[i] >= 'A' and this->content_[i] <= 'Z')
+                res[i] = this->content_[i] + ('a' - 'A');
+        }
+        return res;
+    }
 };
 
 std::ostream& operator<<(std::ostream& os, const String& obj)
@@ -40,9 +80,28 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
     return os;
 }
 
+char& String::operator[](int index) {
+    assert(index < size_);
+    return content_[index];
+}
+
+String& String::operator+(String right) {
+    String res = String(this->len() + right.len());
+    for (int i = 0; i < this->size_; i++){
+        res[i] = this->content_[i];
+        printf("%c", res[i]);
+    }
+    for (int i = 0; i < right.size_; i++) {
+        res[i] = right.content_[i];
+        printf("%c", res[i]);
+    }
+    return res;
+}
+
 int main(){
-    String teststr( 8);
-    String test2(10);
-    std::cout << teststr << test2;
+    String teststr("cock", 4);
+    String test2("check", 5);
+    String a = teststr + test2;
+    //std::cout << teststr + test2.lower();
     return 0;
 }
