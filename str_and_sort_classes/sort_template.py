91a923dabad226c96d78816355c9d48f0ad9fc51
abebus <anamev263@gmail.com>
2022-11-03 00:16:09 +0300

    я правильно понимаю?

--- str_and_sort_classes/sort_template.py
+++ str_and_sort_classes/sort_template.py
@@ -1,12 +1,12 @@
 import abc, random
 
 
-class SortStrategy(abc.ABC):
+class SortStrategy(abc.ABC):  # абстрактный класс
     @abc.abstractmethod
-    def __init__(self, arr, size):
+    def __init__(self, arr, size):  # пустой конструктор
         return
 
-    @abc.abstractmethod
+    @abc.abstractmethod  # пустой деструктор
     def __del__(self):
         return
 
@@ -19,30 +19,36 @@ def is_sorted(arr):
 
 
 class BogoSort(SortStrategy):
-    def __init__(self, arr, size):
-        super().__init__(arr, size)
-        print('bogosort called')
+    def __init__(self, arr, size):  # пустой конструктор, который мы переопределили
+        print('bogosort called')  # так и не вызывается кста
 
-    def __del__(self):
+    def __del__(self):  # пустой деструктор, который мы переопределили
         return
 
     @staticmethod
-    def sort(arr, size):
+    def sort(arr, size):  # алгоритм сортировки. статичный потому что всё равно у класса своих полей нет
         print('sort method called')
         while not is_sorted(arr):
             random.shuffle(arr)
 
 
 class Sort:
-    def __init__(self):
+    def __init__(self):  # пустой конструктор
         return
 
+    def __del__(self):
+        return
+
+    # из класса сорт вызываем сеттер, указывающий на наследника сорт стратеджи
     @staticmethod
-    def set_strategy(strategy: SortStrategy.__subclasses__()):
+    def set_strategy(strategy: SortStrategy.__subclasses__()):  # ооп функциональное метапрогромирование
         return strategy
 
 
 if __name__ == '__main__':
     testarr = [1, 3, 2, 4]
-    Sort.set_strategy(BogoSort).sort(testarr, len(testarr))
+    sort_algorythm = Sort.set_strategy(BogoSort)  # выбрали
+    sort_algorythm.sort(testarr, len(testarr))  # вызвали метод сорт у нужного класса
     print(testarr)
+    # оно нет хз
+    # я бы просто написал класс сорт и у него добавил бы разные методы сортировок без етой вот абстракщины
