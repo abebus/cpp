7d396a62263102c64d75ebda433319bfde9d0696
abebus <anamev263@gmail.com>
2022-11-07 23:40:00 +0300

    sad

--- six_tasks/sixth.cpp
+++ six_tasks/sixth.cpp
@@ -1,11 +1,36 @@
 #include <fstream>
 
 class Matrix{
-    int *row_{};
+    int width_, height_;
     int **rows_{};
 public:
     Matrix()=default;
-    Matrix(int *row,int **rows){
-        //
+    ~Matrix()=default;
+    Matrix(int n,int m){
+        width_ = n;
+        height_ = m;
+        rows_ = new int*[height_]{};
+        for (int i = 0; i < height_; ++i)
+            rows_[i] = new int[width_]{};
+    }
+    Matrix(double scalar, int n){
+        Matrix(n, n);
+        for (int i = 0; i < n; ++i)
+            rows_[i][i] = scalar;
+    }
+
+    Matrix operator*(int scalar){
+        for (int i = 0; i < height_; ++i)
+            for (int j = 0; j < width_; ++j)
+                rows_[i][j] *= scalar;
+    }
+
+    Matrix operator*(Matrix right){
+        Matrix res = Matrix(this->width_, right.height_);
+        for (int i = 0; i < height_; ++i) {
+            for (int j = 0; j < width_; ++j) {
+                res.rows_[i][j] = 1;//todo
+            }
+        }
     }
 };
