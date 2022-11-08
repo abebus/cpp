ad6b6cea0b19621287885cbbb2e50f0eb0058292
abebus <anamev263@gmail.com>
2022-11-08 23:10:11 +0300

    sad

--- six_tasks/sixth.cpp
+++ six_tasks/sixth.cpp
@@ -1,73 +1,81 @@
 #include <fstream>
 #include <iostream>
+#include <cassert>
 
 class Matrix{
-    int width_, height_;
-    int **rows_{};
+    int width_{}, height_{};
+    double **rows_{};
 public:
     Matrix()=default;
     ~Matrix()=default;
     Matrix(int n,int m){
         width_ = n;
         height_ = m;
-        rows_ = new int*[height_]{};
+        rows_ = new double*[height_]{};
         for (int i = 0; i < height_; ++i)
-            rows_[i] = new int[width_]{};
+            rows_[i] = new double [width_]{};
     }
-    Matrix(double scalar, int n){
-        Matrix(n, n);
+    Matrix(double scalar, int n) : Matrix(n, n){
         for (int i = 0; i < n; ++i)
             rows_[i][i] = scalar;
     }
-    Matrix(int **rows, int n, int m){
-        width_ = n;
-        height_ = m;
-        for (int i = 0; i < width_; ++i) {
-            for (int j = 0; j < height_; ++j) {
+    Matrix(int **rows, int n, int m) : Matrix(n, m){
+        for (int i = 0; i < height_; ++i)
+            for (int j = 0; j < width_; ++j)
                 rows_[i][j] = rows[i][j];
-            }
-        }
     }
 
     Matrix operator*(int scalar){
-        auto res = Matrix();//todo
+        auto res = Matrix(width_, height_);
         for (int i = 0; i < height_; ++i)
             for (int j = 0; j < width_; ++j)
-                rows_[i][j] *= scalar;
+                res.rows_[i][j] = rows_[i][j] * scalar;
+        return res;
     }
 
     Matrix operator*(Matrix right){
-        Matrix res = Matrix(this->width_, right.height_);
-
-        for (int i = 0; i < height_; ++i) {
-            for (int j = 0; j < width_; ++j) {
-                for (int k = 0; k < right.height_; ++k) {
-                    for (int l = 0; l < right.width_; ++l) {
-                        res.rows_[i][j] += this->rows_[k][l] * right.rows_[l][k];
-                    }
-                }
-            }
-        }
+        assert(height_ == right.width_);
+        auto res = Matrix(this->height_, right.width_);
+        for (int i = 0; i < height_; ++i)
+            for (int j = 0; j < right.width_; ++j)
+                for (int k = 0; k < right.width_; ++k)
+                    res.rows_[i][j] += this->rows_[k][j] * right.rows_[i][k];
         return res;
     }
+
+    Matrix operator+(Matrix right){
+        assert(width_ == right.width_ and height_ == right.height_);
+        auto res = Matrix(width_, height_);
+        for (int i = 0; i < height_; ++i)
+            for (int j = 0; j < width_; ++j)
+                res.rows_[i][j] = rows_[i][j] + right.rows_[i][j];
+        return res;
+    }
+
     void printMatrix(){
         for (int i = 0; i < height_; ++i) {
-            for (int j = 0; j < width_; ++j) {
+            for (int j = 0; j < width_; ++j)
                 std::cout << rows_[i][j] << ' ';
-            }
             std::cout << '\n';
         }
     }
+
 };
 
 int main(){
-    int a[3]={1, 2, 3};
-    int b[3]={3, 2, 1};
-    int *A[3]={a, a, b};
-    int *B[3]={a, b,b};
-    auto M = Matrix(A, 3, 3);
-    auto N = Matrix(B, 3, 3);
-    auto RES = M * N;
+    int a[2]={1, 0};
+    int b[2]={0, 1};
+    int c[2]={3, 4};
+    int *A[3]={a,
+               b,
+               c};
+    int *B[2]={c,
+               b};
+    auto M = Matrix(A, 2, 3);
+    M.printMatrix();
+    auto N = Matrix(B, 2, 2);
+    N.printMatrix();
+    auto RES = N * M;
     RES.printMatrix();
     return 0;
 }
